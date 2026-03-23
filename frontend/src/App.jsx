e31cf75464cc2370f3ff25b67746c1312cf610c3
vishwizard <deyamrit959@gmail.com>
2026-03-23 14:05:46 +0530

    Testing stream

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-import { SignInButton } from '@clerk/react'
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+
+
 
 function App() {
 
   return (
     <>
-      <h1>Welcome to sign in page</h1>
-      <SignInButton mode="modal"/>
+      <h1>My App</h1>
+      
+      <Show when="signed-out">
+          <SignInButton />
+          <span className='mt-10'>Empty Space</span>
+          <SignUpButton />
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
     </>
   )
 }
