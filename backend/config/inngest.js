3b3d157a96f6d1e99443dc67f7ed865c7a1c3ae1
vishwizard <deyamrit959@gmail.com>
2026-03-23 16:15:25 +0530

    Haan mai main me bhi changes krta hu

--- backend/config/inngest.js
+++ backend/config/inngest.js
@@ -3,45 +3,40 @@ import User from "../models/User.js";
 import { connectDB } from "./db.js";
 import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
-export const inngest = new Inngest({ id: "hireorfire" });
+export const inngest = new Inngest({id:"hireorfire"});
 
 const syncUser = inngest.createFunction(
-    { id: "sync-user-from-clerk" }, 
-    { events: ["clerk/user.created", "clerk/user.updated"] },
-    async ({ event }) => {
+    {id:"create-user"},
+    {event: "clerk/user.created"},
+    async ({event})=>{
         await connectDB();
 
-        const { id, email_addresses, first_name, last_name, image_url } = event.data;
-        const fullName = `${first_name || ""} ${last_name || ""}`.trim();
-        const email = email_addresses[0]?.email_address;
+        const {id, email_addresses, first_name, last_name, image_url} = event.data;
+        const user = new User({
+            clerkId:id,
+            name:`${first_name || ""} ${last_name || ""}`,
+            email:email_addresses[0]?.email_address,
+            profileImage:image_url,
+        });
 
-        await User.findOneAndUpdate(
-            { clerkId: id },
-            {
-                clerkId: id,
-                name: fullName,
-                email: email,
-                profileImage: image_url,
-            },
-            { upsert: true, new: true }
-        );
-
+        await User.save(user);
         await upsertStreamUser({
-            id: id.toString(),
-            name: fullName,
-            image: image_url
+            id:user.clerkId.toString(),
+            name:user.name,
+            image:user.profileImage
         });
     }
 );
 
 const deleteUserFromDB = inngest.createFunction(
-    { id: "delete-user" },
-    { event: "clerk/user.deleted" },
-    async ({ event }) => {
+    {id:"delete-user"},
+    {event: "clerk/user.deleted"},
+    async ({event})=>{
         await connectDB();
-        const { id } = event.data;
 
-        await User.deleteOne({ clerkId: id });
+        const {id} = event.data;
+
+        await User.deleteOne({clerkId:id});
         await deleteStreamUser(id.toString());
     }
 );
