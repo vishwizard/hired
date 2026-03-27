d5f0552af58a8fcf4efa9d397e185425ce4d2787
vishwizard <deyamrit959@gmail.com>
2026-03-27 19:14:34 +0530

    Setup daisy ui and tanstack

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
-
-
+import { Navigate, Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage.jsx'
+import AboutPage from './pages/AboutPage.jsx'
+import ProblemsPage from './pages/ProblemsPage.jsx'
+import { Toaster } from 'react-hot-toast';
 
 function App() {
-
+  const {isSignedIn} = useUser();
   return (
     <>
-      <h1>My App</h1>
+      <h1 className='bg-red-500 text-4xl'> My App</h1>
+      <button className='btn btn-primary'>Hit me Daddy</button>
       
-      <Show when="signed-out">
-          <SignInButton />
-          <span className='mt-10'>Empty Space</span>
-          <SignUpButton />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
+      <Routes>
+        <Route path='/' element={<HomePage/>}> </Route>
+        <Route path='/about' element={<AboutPage/>}> </Route>
+        <Route path='/problems' element={isSignedIn ? <ProblemsPage/> : <Navigate to={"/"}/>}></Route>
+      </Routes>
+      <Toaster toastOptions={{duration:3000}}/>
     </>
   )
 }
---
+++ frontend/src/pages/ProblemsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProblemsPage = () => {
+  return (
+    <div>
+      I am Problem
+    </div>
+  )
+}
+
+export default ProblemsPage
